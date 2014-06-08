646ca5dd4f982b3f3bc23461ad3138256addc13c
teppei machida <machida@fjord.jp>
2014-06-08 10:47:54 +0900

    Update

--- lib/middleman-oulu.rb
+++ lib/middleman-oulu.rb
@@ -1,7 +1,13 @@
 require "middleman-oulu/version"
+require 'sass'
 
 module Middleman
   module Oulu
-    # Your code goes here...
+    config.compass.require "rgbapng"
+    config.compass.require 'SassyLists'
+    config.compass.require 'ceaser-easing'
+    config.compass.require 'breakpoint'
+    config.compass.require 'modular-scale'
+    config.require 'sassy-maps'
   end
 end
