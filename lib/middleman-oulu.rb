5ce44561c50aecbc2940432740b95d975064bc8b
teppei machida <machida@fjord.jp>
2014-06-10 03:45:52 +0900

    Remove config

--- lib/middleman-oulu.rb
+++ lib/middleman-oulu.rb
@@ -3,11 +3,5 @@ require 'sass'
 
 module Middleman
   module Oulu
-    config.compass.require "rgbapng"
-    config.compass.require 'SassyLists'
-    config.compass.require 'ceaser-easing'
-    config.compass.require 'breakpoint'
-    config.compass.require 'modular-scale'
-    config.require 'sassy-maps'
   end
 end
--- lib/middleman-oulu/version.rb
+++ lib/middleman-oulu/version.rb
@@ -1,5 +1,5 @@
 module Middleman
   module Oulu
-    VERSION = "0.0.4"
+    VERSION = "0.0.5"
   end
 end
